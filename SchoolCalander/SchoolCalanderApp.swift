e352913484d029f881dd8a9df1cdece1d1e4b9f1
Sebastian Frosch <sfrosch@lcs.on.ca>
2023-05-30 19:34:24 -0400

    Initial Commit

---
+++ SchoolCalander/SchoolCalanderApp.swift
@@ -0,0 +1,17 @@
+//
+//  SchoolCalanderApp.swift
+//  SchoolCalander
+//
+//  Created by Sebastian Frosch on 30.05.23.
+//
+
+import SwiftUI
+
+@main
+struct SchoolCalanderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
