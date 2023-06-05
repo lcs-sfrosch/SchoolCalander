2bbc9e6ca8b3476a346e9a611752d478f656b863
Sebastian Frosch <sfrosch@lcs.on.ca>
2023-06-05 09:23:12 -0400

    Fixed error in calendar preview structure

--- SchoolCalander/Views/CalendarView.swift
+++ SchoolCalander/Views/CalendarView.swift
@@ -12,36 +12,31 @@ struct calendarView: View {
     let items = ["Item 1", "Item 2", "Item 3", "Item 4", "Item 5", "Item 6", "Item 7", "Item 8", "Item 9", "Item 10"]
     
     var body: some View {
-        
-        VStack{
-            HStack{
-                Text("Calander")
+        VStack {
+            HStack {
+                Text("Calendar")
                     .font(.largeTitle)
                 Spacer()
             }
             
-            VStack{
+            VStack {
                 List {
                     ScrollView {
-                        
-                        ForEach(items.indices, id: \.self){ index in
+                        ForEach(items.indices, id: \.self) { index in
                             RoundedRectangle(cornerRadius: 10)
                                 .strokeBorder(Color.black, lineWidth: 2)
                         }
                     }
                 }
-                
             }
-            .frame(width:350, height:500)
+            .frame(width: 350, height: 500)
             .cornerRadius(10)
-            
-            
         }
-        
-        struct calendarView_Previews: PreviewProvider {
-            static var previews: some View {
-                calendarView()
-            }
-        }
-        
-        
+    }
+}
+
+struct calendarView_Previews: PreviewProvider {
+    static var previews: some View {
+        calendarView()
+    }
+}
