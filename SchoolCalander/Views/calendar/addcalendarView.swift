b8889c1ab40a3373eeb386d6380c786e537b3dc9
Sebastian Frosch <sfrosch@lcs.on.ca>
2023-06-09 12:56:46 -0400

    added picker menu

--- SchoolCalander/Views/calendar/addcalendarView.swift
+++ SchoolCalander/Views/calendar/addcalendarView.swift
@@ -9,24 +9,51 @@ import SwiftUI
 
 struct addcalendarView: View {
     
-    @State var name: String = "Math"
+    @State var name: String = ""
     @State var date: Int = 1
-    @State var description: String = "Math Homework"
-
+    @State var description: String = ""
+    
+    @Environment(\.blackbirdDatabase) var db: Blackbird.Database?
     
     var body: some View {
         
         VStack {
-            Text("Add Calendar Item")
+            HStack{
+                Picker("Classes", selection: $name) {
+                    ForEach(name, id: \.self) {
+                        Text(name)
+                            .frame(height: 30)
+                    }
+                }
+                .pickerStyle(.menu)
+            }
+            HStack{
+                TextField("description", text: $description)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .frame(height: 30)
+                
+            }
+            
+            Button(action: {
+                Task{
+                    try await db!.transaction { core in
+                        try core.query("INSERT INTO Class (name, descriton) VALUES (?)", name, description)
+                        
+                    }
+                }
+                name = ""
+            }, label: {
+                    Text("ADD")
+                    .font(.title2)
+                
+            })
+            }
+                   }
+                   struct addcalendarView_Previews: PreviewProvider {
+                static var previews: some View {
+                    addcalendarView()
+                        .environment(\.blackbirdDatabase,AppDatabase.instance)
+                }
+            }
         }
-        
-        
-    }
-}
-struct addcalendarView_Previews: PreviewProvider {
-    static var previews: some View {
-        addcalendarView()
-            .environment(\.blackbirdDatabase,AppDatabase.instance)
-    }
-}
-
+    
